1d85031d8571ee11db7262bca187071b30affe34
Joel Daniel Rico <joeldanielrico@gmail.com>
2023-09-26 03:12:07 -0700

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
+import uvicorn
+import os
+
+
+load_dotenv()
+os.makedirs('./static', exist_ok=True)
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
