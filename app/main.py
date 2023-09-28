45d744f8571554f50548647dacfffa70afd4b1d6
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-09-28 02:39:08 -0700

    add api and markdown support

--- app/main.py
+++ app/main.py
@@ -2,23 +2,25 @@ from fastapi import FastAPI, Request, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
+import google.generativeai as palm
 from dotenv import load_dotenv
 from uploads import UploadType
 from pypdf import PdfReader
 import uvicorn
 import os
+import markdown
 
 
 load_dotenv()
+PALM_API_KEY = os.getenv("API_KEY")
+
+palm.configure(api_key=PALM_API_KEY)
 os.makedirs('./static', exist_ok=True)
 os.makedirs('./uploads', exist_ok=True)
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -46,7 +48,26 @@ async def upload(request: Request, file: UploadFile):
     finally:
         file.file.close()
 
-    return templates.TemplateResponse("file.html", {"request": request, "file_name": file.filename, "content": content})
+    if content == "":
+        content = "Failed to extract text"
+        res = ""
+        return templates.TemplateResponse("file.html", {"request": request, "file_name": file.filename, "content": content, "ai": res})
+
+    question = "Describe the following text"
+    prompt = f"""
+    {question}:
+
+    {content}
+    """
+
+    res = palm.generate_text(
+        prompt=prompt,
+        temperature=0.5,
+        max_output_tokens=600,
+    ).result
+    res = markdown.markdown(res)
+
+    return templates.TemplateResponse("file.html", {"request": request, "file_name": file.filename, "content": content, "ai": res})
 
 
 if __name__ == "__main__":
